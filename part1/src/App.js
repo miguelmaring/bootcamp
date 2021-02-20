8067000c21b2845f475d5f2b7be99667f508b9ab
miguelmaring <miguelmaring@gmail.com>
2021-02-20 23:44:36 +0100

    commit

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       Hola Mundo
+      <Footer></Footer>
     </div>
   );
 }
